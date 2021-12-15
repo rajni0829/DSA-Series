a9653f4b6d6ff3df3df674b4b3a52385f05889e0
Rajni Gupta <rajnigupta2901@gmail.com>
2021-12-15 17:03:02 +0530

    DSA added

---
+++ PowerOf2.cpp
@@ -0,0 +1,19 @@
+
+
+int main(){
+    int ans = 1;
+    int n;
+    cin >> n;
+    bool bol;
+     for(int i = 0; i <= 30; i++){
+         if(ans == n){
+             bol =  true;
+         }
+         if (ans < (INT_MAX/2)){
+
+          ans = ans*2;
+         }
+
+     }
+        bol = false;
+}
